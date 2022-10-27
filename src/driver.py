717e8682b2acba5e19297efbfa96ff3f2f27b2d8
Tyler Conley <tyler.conley@sdl.usu.edu>
2022-10-27 17:42:50 -0600

    updated README

--- src/driver.py
+++ src/driver.py
@@ -5,7 +5,9 @@ env = kEnv()
 
 state, _ = env.reset()
 
-print(state)
+print('--- Key Scores with QWERTY ---')
+for i in state:
+    print(f'{i}: {round(state[i], 2)}')
 
 action = [                                         ['a', 'b'],
 ['c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'],
@@ -15,5 +17,20 @@ action = [                                         ['a', 'b'],
 
 state, reward, done, *_ = env.step(action)
 
-print(state)
-print(reward)
+print('\n--- Key Scores with Alphabetical Order ---')
+for i in state:
+    print(f'{i}: {round(state[i], 2)}')
+print(f'Reward for Alphabetical: {reward}')
+
+action = [                                         ['-', '='],
+['/', ',', '.', 'p', 'y', 'f', 'g', 'c', 'r', 'l', '[', ']', '\\'],
+  ['a', 'o', 'e', 'u', 'i', 'd', 'v', 't', 'n', 's', '\''],
+    [';', 'q', 'j', 'k', 'x', 'b', 'm', 'w', 'v', 'z']
+]
+
+state, reward, done, *_ = env.step(action)
+
+print('\n--- Key Scores with Dvorak ---')
+for i in state:
+    print(f'{i}: {round(state[i], 2)}')
+print(f'Reward for Alphabetical: {reward}')
